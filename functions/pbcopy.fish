543bddf40f1e0676dc5baa4ba8aa285654c7af01
Bryan Alexander Rivera <futurehorizon@gmail.com>
2018-02-11 16:06:59 -0500

    Update pbcopy.fish

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -1,4 +1,4 @@
-if not command -q pbcopy >/dev/null ^&1
+if not type -q pbcopy >/dev/null ^&1
   function pbcopy --description "Copy data from STDIN to the clipboard"
       xsel --clipboard --input
   end
